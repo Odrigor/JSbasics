710d30d884ffdd22090adb5fc4b2de40b7a3e7dc
Odrigor <rodcid.al@gmail.com>
2022-07-26 03:08:29 -0400

    codigo comentado

--- validaEmail/index.js
+++ validaEmail/index.js
@@ -1,31 +1,35 @@
 
-const cont = document.querySelector('#email');  // hacemos un querySelector,
-const mensaje = document.querySelector('#mensaje');
+const cont = document.querySelector('#email');  // hacemos un querySelector, para traer ele elemntoq ue tenga el valor del atributo id, si fueraa por clases seria .email
+const mensaje = document.querySelector('#mensaje'); // traemis desde el html el elemnto, que tenga el valor del atributo id, mensaje, si fuera por clases deberia ser .mensaje
 
-eventListeners();
+eventListeners();   // ejecutamos la funcion que contiene los event listeners
 
 function eventListeners()
 {
-    document.addEventListener('submit', (e)=> e.preventDefault())
-    cont.addEventListener('blur', validaEmail);
+    document.addEventListener('submit', (e)=> e.preventDefault()) // con esta linea, desactivamos que al presionar enter, se recarge la pagina
+    cont.addEventListener('blur', validaEmail); // al momento de clickear fuera del contenedor, se ejecutara la funcion, valida email
 }
 
 function validaEmail(e)
 {
+    /**
+     * par validar e mails de manera profesional, se usan expresiones regulares, la mayoria del tiempo ya estan escritas y cuestion
+     * de buscarlas por internet, para validar email usaremos https://www.emailregex.com/
+     */
     const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     if(er.test( e.target.value))
     {
         console.log('mail valido')
-        const error = document.querySelector('p.error');
-        if(!error){
+        const error = document.querySelector('p.error'); // aqui buscamos si existe el elemento html, que avisa de que habia un error en el mail, para proceder a eliminarlo
+        if(error!==null){
             error.remove();
         }
         
     }
     else{
         console.log('mail no valido');
-        mostrarError('mail no valido :D');
+        mostrarError('mail no valido :D'); // en caso de que no sea valido, agregamos un html, que dice que el mail no es valido
     }
     console.log('validando');
 }
